810198c80873ed2167cc15ebdcd5217ff16d76fc
June <59625251+sukjunel@users.noreply.github.com>
2020-12-31 22:15:11 +0000

    init DrugDisc Project

--- DrugDisc/main.py
+++ DrugDisc/main.py
@@ -4,13 +4,22 @@ from chembl_webresource_client.new_client import new_client
 target = new_client.target
 target_query = target.search("coronavirus")
 targets = pd.DataFrame.from_dict(target_query)
-print(targets)
+#print(targets)
 
 selected_target = targets.target_chembl_id[4]
-print(selected_target)
+#print(selected_target)
 
 activity = new_client.activity
 res = activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50")
 res_df = pd.DataFrame.from_dict(res)
-print(res_df)
+#print(res_df)
 
+#res_df.to_csv('bioactivity_data.csv', index=False)
+
+csvfile = pd.read_csv('bioactivity_data.csv')
+
+print (csvfile.head)
+
+data = res_df[res_df.standard_value.notna()]
+
+//
